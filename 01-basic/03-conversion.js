821451f9f1e0cde7611176d69022a42f8c4836e5
Abhinav Singh <137477012+abhinav5oct@users.noreply.github.com>
2024-04-11 18:46:56 +0000

    conversion

---
+++ 01-basic/03-conversion.js
@@ -0,0 +1,24 @@
+let sore = "33";
+
+console.log(typeof sore)
+
+console.log(typeof (sore))
+
+let valueInNumber = Number(sore);
+console.log(typeof valueInNumber);
+
+
+let isLoggedIn = 2;
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanIsLoggedIn)
+
+console.log(typeof BooleanIsLoggedIn)
+
+let someNumber = 55;
+
+let someString = String(someNumber);
+
+console.log(someString)
+
+console.log(typeof someString)
